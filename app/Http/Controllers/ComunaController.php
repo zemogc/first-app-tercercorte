af9531871945aea9f51b65a199393a3f3f3f956e
Brian Marin <brian200218@gmail.com>
2024-03-28 15:08:58 -0500

    Ajuste de la vista

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -12,10 +12,14 @@ class ComunaController extends Controller
      * Display a listing of the resource.
      */
 
-     
+
     public function index()
     {
-        $comunas = Comuna::all();
+        //$comunas = Comuna::all();
+        $comunas = DB::table('tb_comuna')
+            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+            ->get();
         return view('comuna.index', ['comunas'=>$comunas]);
     }
 
--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -29,7 +29,7 @@
         <tr>
             <th scope="row">{{ $comuna->comu_codi }}</th>
             <td>{{ $comuna->comu_nomb }}</td>
-            <td>{{ $comuna->muni_codi }}</td>
+            <td>{{ $comuna->muni_nomb }}</td>
             <td><span> Actions </span></td>
         </tr>
         @endforeach
