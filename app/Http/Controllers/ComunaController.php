a8eb987911c61e8d6435be472dc3d1795da3b1dd
Brian Marin <brian200218@gmail.com>
2024-03-31 22:20:54 -0500

    Add Comuna

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -28,7 +28,10 @@ class ComunaController extends Controller
      */
     public function create()
     {
-        //
+        $municipios = DB::table('tb_municipio')
+        ->orderBy('muni_nomb')
+        ->get();
+        return view('comuna.new', ['municipios' => $municipios]);
     }
 
     /**
@@ -36,7 +39,18 @@ class ComunaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $comuna = new Comuna();
+        // $comuna->comu_codi = $request->id;
+        // El código de comuna es auto incremental
+        $comuna->comu_nomb = $request->name;
+        $comuna->muni_codi = $request->code;
+        $comuna->save();
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+        ->get();
+        return view('comuna.index', ['comunas' => $comunas]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/comunas', [ComunaController::class, 'index']);
+Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas.index');
+Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
+Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
 
-Route::get('/comunas/create', 'ComunasController@create')->name('comunas.create');
+//Route::get('/comunas/create', 'ComunasController@create')->name('comunas.create');
 
 
 /*Route::get('/estudiantes', function () {
