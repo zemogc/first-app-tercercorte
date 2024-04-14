acb596d22d9acb8f5396856fcbdd7ce9323b4b4b
Brian Marin <brian200218@gmail.com>
2024-04-14 17:43:53 -0500

    Taller 3 avance

--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -11,49 +11,56 @@
     <title>COMUNAS</title>
   </head>
   <body>
+  <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Comunas') }}
+        </h2>
+    </x-slot>
     <div class="container">
     <h1>Listado de Comunas</h1>
-    <a href="{{ route('comunas.create') }}" class="btn btn-success">Add</a>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <a href="{{ route('comunas.create') }}"
+                        class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded ml-2">Add</a>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Code</th>
+                                <th scope="col">Commune</th>
+                                <th scope="col">Municipality</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($comunas as $comuna)
+                                <tr>
+                                    <th scope="row">{{ $comuna->comu_codi }}</th>
+                                    <td>{{ $comuna->comu_nomb }}</td>
+                                    <td>{{ $comuna->muni_nomb }}</td>
+                                    <td>
+                                        <a href="{{ route('comunas.edit', ['comuna' => $comuna->comu_codi]) }}"
+                                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                            Edit </a></li>
 
-    <table class="table">
-        <thead>
-            <tr>
-            <th scope="col">Code</th>
-            <th scope="col">Comuna</th>
-            <th scope="col">Municipio</th>
-            <th scope="col">Action</th>
-        </tr>
-        </thead>
-        <tbody>
-            @foreach ($comunas as $comuna)
-        <tr>
-            <th scope="row">{{ $comuna->comu_codi }}</th>
-            <td>{{ $comuna->comu_nomb }}</td>
-            <td>{{ $comuna->muni_nomb }}</td>
-            <td>
-              <a href="{{route('comunas.edit', ['comuna'=>$comuna->comu_codi])}}"
-              class="btn btn-info"> Edit </a> </li>
-              <form action="{{ route('comunas.destroy', ['comuna'=>$comuna->comu_codi]) }}"
-              method="POST" style="display:inline-block">
-              @method('delete')
-              @csrf
-              <input class="btn btn-danger" type="submit" value="Delete">
-              </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
-</div>
-    <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
+                                        <form action="{{ route('comunas.destroy', ['comuna' => $comuna->comu_codi]) }}"
+                                            method='POST' style="display: inline-block">
+                                            @method('delete')
+                                            @csrf
+                                            <input
+                                                class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2"
+                                                type="submit" value="Delete">
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
